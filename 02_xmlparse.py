fb4b892f4a84a332d068ec83241226ce07924ad4
gangand <dmitriigangan.reg@gmail.com>
2016-03-04 22:39:00 +0200

    XML parser orking pretty good.
    Need to add userfriendly menu, do class/classes and methods to save different results to file

--- 02_xmlparse.py
+++ 02_xmlparse.py
@@ -1,5 +1,12 @@
 import xml.etree.ElementTree as ET
 
+def categ(oid):
+	cat_list=oid.keys()
+	ban_list=['Debug','License Management']
+	for item in ban_list:
+		cat_list.remove(item)
+	return cat_list
+
 def oid(category):
 	col_width = max(len(word) for row in oids[category].items() for word in row) + 4
 	for key in oids[category].items():
@@ -9,14 +16,29 @@ def print_cat(cat_list):
 	for item in cat_list:
 		print "\t",item
 	cat=raw_input("\nEnter category: ")
-	oid(cat)
-file=open('Access_06.55.00.74_Template.xml')
+	return cat
+
+def get_file(prompt, mode):
+    while True:
+        try:                                    
+            return open(raw_input(prompt), mode) 
+        except OSError as error:
+            print error
+	
+file=get_file('Enter filename: ', 'r')
 data=file.read()
 tree = ET.fromstring(data)
 xml = tree.findall('.Element')
-print type(xml)
+
+oids={}
 for element in xml:
+	oids[element.get('Name')]={}
 	a=element.findall('Element/Parameters/Parameter')
 	for item in a:
-		print item.get('ID') 
-		print item.find('Visualization/Name').text
+		oids[element.get('Name')][item.find('Visualization/Name').text]=item.get('ID') 
+
+		
+cat_list=categ(oids)
+
+category=print_cat(cat_list)
+oid(category)
